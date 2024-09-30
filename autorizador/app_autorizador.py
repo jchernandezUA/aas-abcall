930877caaf9a69316a3234188e151a62e257f307
nicoGarciaValde <ne.garciav1@uniandes.edu.co>
2024-09-29 20:22:19 -0500

    update union api gateway y autorizacion (#35)
    * update validaciones de autorizacion
    
    * - Se agrega autorizador de tipos de usuario con JWT
    
    ---------
    
    Co-authored-by: nicolas <nico.ing.mec.2@gmail.com>
    Co-authored-by: Jose Carlos Hernandez <jose.hernandez@line-up.com.mx>

--- autorizador/app_autorizador.py
+++ autorizador/app_autorizador.py
@@ -1,4 +1,5 @@
 from base import app, api, Resource, permission_required, create_token, jsonify, request, decode_token
+from flask_jwt_extended import jwt_required, get_jwt
 
 
 
@@ -23,7 +24,29 @@ class VistaLogIn(Resource):
 
 api.add_resource(VistaLogIn, '/login')
 
+class ValidateToken(Resource):
+    @jwt_required()
+    def post(self):
+        data = request.get_json()
+        method = data["method"]
+        print(f"Headers recibidos: {request.headers}")
+        print(f"JSON body recibido: {request.get_json()}")
+        # Obtener los claims del token JWT
+        claims = get_jwt()
+        user_permissions = claims.get("permissions", [])
+
+        print(f"Permisos en el token: {user_permissions}")
+
+        # Verificar si el usuario tiene al menos uno de los permisos requeridos
+        if method in user_permissions:
+            return {"message": "Token and permissions are valid"}, 200
+        else: 
+            return {"message": "Permission denied"}, 401
+
+# Añadir la ruta de validación al microservicio de autorización
+api.add_resource(ValidateToken, '/validate')
+
 if __name__ == '__main__':
-    app.run(ssl_context=('/etc/autorizador/tls/autorizador-cert.pem', '/etc/autorizador/tls/llave.pem'), host='0.0.0.0', port=5005)
+    app.run(ssl_context=('../nginx/tls/autorizador-cert.pem', '../nginx/tls/llave.pem'), host='0.0.0.0', port=5005)
 
 
